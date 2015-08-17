cde199eb5d5e7e2311d17150db1d6ee27fb05fd1
Valentina Cupać <valentina.cupac@optivem.com>
2015-08-17 20:47:40 +0200

    Updated assembly details

--- Optivem.Vembridge.Tests/Properties/AssemblyInfo.cs
+++ Optivem.Vembridge.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Viva.Dsv.Test")]
+[assembly: AssemblyTitle("Optivem.Vembridge.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Viva.Dsv.Test")]
+[assembly: AssemblyCompany("Optivem")]
+[assembly: AssemblyProduct("Optivem.Vembridge.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Optivem.Vembridge/Properties/AssemblyInfo.cs
+++ Optivem.Vembridge/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Viva.Dsv")]
+[assembly: AssemblyTitle("Optivem.Vembridge")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Viva.Dsv")]
+[assembly: AssemblyCompany("Optivem")]
+[assembly: AssemblyProduct("Optivem.Vembridge")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
